44be9d6c0226b67297df4878155dc23f336d35c9
Arup Sen <arupsen48@gmail.com>
2019-10-14 09:39:30 +0100

    create test for date

--- five_minute_journal_test.rb
+++ five_minute_journal_test.rb
@@ -4,7 +4,8 @@ require_relative 'five_minute_journal'
 class FiveMinuteJournalTest < Minitest::Test
   def test_displays_todays_date
     # skip
-    expected = { 'B' => 9, 'I' => 1, 'L' => 0 }
-    assert_equal expected, FiveMinuteJournal.to_s
+    today = Time.new
+    expected = "#{today.day.to_s + '-' + today.month.to_s + '-' + today.year.to_s}"
+    assert_equal expected, FiveMinuteJournal.new.date
   end
 end
